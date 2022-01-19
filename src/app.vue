e90801df5b092f338f61fb66522868d651a3e51a
Charlon Palacay <charlon@gmail.com>
2022-01-18 17:30:20 -0800

    Initial commit

---
+++ src/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="bg-light text-center">
+    <img alt="Vue logo" src="./assets/logo.png" class="inline" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+  <main class="p-12">
+    <router-view />
+  </main>
+  <footer class="bg-secondary text-center">
+    this is the footer
+  </footer>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './app.vue'
+import router from './router' 
+
+// bootstrap 4 (standalone)
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+// tailwind
+// import './index.css'
+
+createApp(App).use(router).mount('#app')
